6148cd0e25adaa4ba84f4265f65330edcef901cb
Jimma Shanko <jshanko754@gmail.com>
2024-10-27 18:51:09 -0500

    cdk

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,14 +1,13 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+import aws_cdk as aws_cdk
+import aws_cdk.aws_s3 as aws_s3
+
 from constructs import Construct
 
-class HelloCdkStack(Stack):
+class HelloCdkStack(cdk.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
 
         # The code that defines your stack goes here
 
